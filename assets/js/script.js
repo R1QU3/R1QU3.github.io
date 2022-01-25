110fd24ca37b70cae9233e3427d873de5a3ca2a5
R1QU3 <josehenriquehyttspf@gmail.com>
2022-01-25 12:05:53 -0300

    add js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+/* Scroll */
+function initScrollSmooth() {
+  const links = document.querySelectorAll('.js-menu a[href^="#"]')
+  if (links.length) {
+    function scrollToLink(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href)
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      })
+    }
+    links.forEach((link) => {
+      link.addEventListener('click', scrollToLink)
+    })
+  }
+}
+initScrollSmooth()
